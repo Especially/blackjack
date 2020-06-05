93a39fffe0ab0cb2bffd238153c99cc45d35c5c9
Especially <_j@live.ca>
2020-06-05 12:43:26 -0400

    Draw cards func

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -10,8 +10,13 @@ class Main extends React.Component {
     state = {
         deckID: null,
         bet: 0,
-        dealer: [],
-        player: []
+        notice: '',
+        dealerCards: [],
+        playerCards: [],
+        dealerCount: 0,
+        playerCount: 0,
+        wallet: 500,
+        gameOver: false
     }
 
     newGame() {
@@ -25,15 +30,51 @@ class Main extends React.Component {
             })
     }
 
-    drawCards(newGame) {
+    evaluateCard(card) {
+        if (!Number(card.value)) {
+            console.log('Not a number', card.value);
+            if (card.value === ('JACK','QUEEN','KING')) {
+                const val = 10;
+            }
+
+        } else {
+            console.log(card.value);
+        }
+
+    }
+    gameReset() {
+        this.setState({deckID: null,
+            bet: 0,
+            notice: '',
+            dealerCards: [],
+            playerCards: [],
+            dealerCount: 0,
+            playerCount: 0,
+            gameOver: false})
+    }
+
+    drawCards(newGame, player) {
         if (newGame) {
             axios.get(`${API_URL}/${this.state.deckID}/draw/?count=4`)
                 .then(res => {
                     const cards = res.data.cards;
                     const dealer = [cards[0], cards[2]];
                     const player = [cards[1], cards[3]];
-                    this.setState({dealer, player})
-                    console.log(this.state);
+                    this.setState({ dealer, player });
+                    this.state.dealer.forEach( item => {
+                        this.evaluateCard(item);
+                    })
+                }).catch(err => console.log(err));
+        }
+        else {
+            axios.get(`${API_URL}/${this.state.deckID}/draw/?count=1`)
+                .then(res => {
+                    const card = res.data.cards;
+                    const cards = (player === 'user') ? this.state.playerCards : this.state.dealerCards;
+                    const newCards = [...cards, card];
+                    console.log(newCards);
+                    (player === 'user') ? this.setState({playerCards: newCards}) : this.setState({dealerCards: newCards});
+
                 })
         }
     }
@@ -43,7 +84,7 @@ class Main extends React.Component {
         return (
             <>
                 <Dealer deckID={this.state.deckID}></Dealer>
-                <Player deckID={this.state.deckID} newGame={this.newGame.bind(this)}></Player>
+                <Player deckID={this.state.deckID} newGame={this.newGame.bind(this)} drawCards={this.drawCards.bind(this)}></Player>
             </>
         )
     }
